f8214beacdce26ed5fdf031a838d635fe503d61e
block-expert <luanswork@gmail.com>
2023-04-18 11:31:08 -0400

    update qstash

---
+++ pages/api/qstash.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { verifySignature } from "@upstash/qstash/nextjs";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.headers);
+
+  await new Promise((r) => setTimeout(r, 1000));
+
+  console.log("Success");
+  console.log(typeof req.body, { body: req.body });
+  res.status(200).json({ name: "John Doe", body: req.body });
+}
+
+export default verifySignature(handler, {
+  currentSigningKey: "sig_6QJLULeiJf6La81Nf86hNFXJpNWd",
+  nextSigningKey: "sig_5qnYiBNjHJNYHYHJRe7dXKctfgGN",
+  url: "http://localhost:3000/api/qstash", // omit this when you deploy
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
